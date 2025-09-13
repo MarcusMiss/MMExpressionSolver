7bfe13cad7e500a3565f14a61b8b15afc073d0fb
Marcus Miss <marcus.miss@mac.com>
2025-09-13 19:24:23 +0200

    Update build_docc.sh

--- build_docc.sh
+++ build_docc.sh
@@ -8,6 +8,6 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/MMExpressionSolver.doccarchive" \
     --output-path "docc" \
-    --hosting-base-path "https://marcusmiss.github.io/MMExpressionSolver/"
+    --hosting-base-path "MMExpressionSolver"
 
 #echo '<!DOCTYPE html><html><head><meta http-equiv = "refresh" content = "0; url = https://marcusmiss.github.io/MMExpressionSolver/documentation/mmexpressionsolver" /><link rel="canonical" href="https://marcusmiss.github.io/MMExpressionSolver/documentation/mmexpressionsolver"></head><body></body></html>' > docc/index.html
